a73b6fd84438dbee4e23986cfec65430a983e6ad
BogdanIrimie <bogdan.irimie@puppet.com>
2020-11-06 10:06:25 +0200

    (FACT-2853) Replace _ with - in help.

--- acceptance/tests/options/help.rb
+++ acceptance/tests/options/help.rb
@@ -9,5 +9,28 @@ test_name "C99984: --help command-line option prints usage information to stdout
         assert_match(/(facter|facter-ng) \[options\] \[query\] \[query\] \[...\]/, stdout, "Expected stdout to contain usage information")
       end
     end
+
+    step "Facter help should list options with minus sign" do
+      options = %w[
+        --no-color
+        --custom-dir
+        --external-dir
+        --log-level
+        --no-block
+        --no-cache
+        --no-custom-facts
+        --no-external-facts
+        --no-ruby
+        --show-legacy
+        --list-block-groups
+        --list-cache-groups
+      ]
+      on(agent, facter('--help')) do
+        options.each do |option|
+          p option
+          assert_match(/#{option}/, stdout, "Key: #{option} not found in help descriptiion")
+        end
+      end
+    end
   end
 end
